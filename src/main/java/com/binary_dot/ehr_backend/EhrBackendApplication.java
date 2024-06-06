a30098d90784c2a34f1d1298ac31b6c265787301
Pabodha <pabodhap@gmail.com>
2024-06-06 18:24:49 +0530

    init

---
+++ src/main/java/com/binary_dot/ehr_backend/EhrBackendApplication.java
@@ -0,0 +1,13 @@
+package com.binary_dot.ehr_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EhrBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EhrBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/binary_dot/ehr_backend/EhrBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.binary_dot.ehr_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EhrBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
